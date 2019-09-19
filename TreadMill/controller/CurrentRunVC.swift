5bb9f241dd2493b26b8134da819ae874bc3e82c0
Noel-96 <noelorion69@gmail.com>
2019-09-19 13:14:27 +0100

    slider button implemented

--- TreadMill/controller/CurrentRunVC.swift
+++ TreadMill/controller/CurrentRunVC.swift
@@ -19,7 +19,37 @@ class CurrentRunVC: LocationVC  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
+        sliderImageView.addGestureRecognizer(swipeGesture)
+        sliderImageView.isUserInteractionEnabled = true
+        swipeGesture.delegate = self as? UIGestureRecognizerDelegate
     }
     
+    
+    @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
+        let minAdjust: CGFloat = 80
+        let maxAdjust: CGFloat = 128
+        if let sliderView = sender.view {
+            if sender.state == UIGestureRecognizer.State.began || sender.state == UIGestureRecognizer.State.changed {
+                let translation = sender.translation(in: self.view)
+                if sliderView.center.x >= (swipeBGImageView.center.x - minAdjust) && sliderView.center.x <= (swipeBGImageView.center.x + maxAdjust) {
+                    sliderView.center.x = sliderView.center.x + translation.x
+                } else if sliderView.center.x >= (swipeBGImageView.center.x + maxAdjust) {
+                    sliderView.center.x = swipeBGImageView.center.x + maxAdjust
+         //           endRun()
+                    dismiss(animated: true, completion: nil)
+                } else {
+                    sliderView.center.x = swipeBGImageView.center.x - minAdjust
+                }
+                
+                sender.setTranslation(CGPoint.zero, in: self.view)
+            } else if sender.state == UIGestureRecognizer.State.ended {
+                UIView.animate(withDuration: 0.1, animations: {
+                    sliderView.center.x = self.swipeBGImageView.center.x - minAdjust
+                })
+            }
+        }
+    }
 
 }
